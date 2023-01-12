0d3b68e6dcb637e94aebb93179aff4e79f3faacf
Ibn-abdillah <akinolasheriff@gmail.com>
2023-01-12 13:26:18 +0100

    added 3 files

---
+++ index.js
@@ -0,0 +1,20 @@
+const counter = document.querySelector('.counter')
+const back = document.querySelector('.load-back')
+const front = document.querySelector('.load-front')
+
+
+let idx = 0;
+
+update()
+
+function update(){
+    counter.innerText = idx + '%'
+    front.style.width = idx +'%'
+    idx++
+    if(idx < 101){
+        setTimeout(update, 40)
+    }
+    if(idx == 100){
+        counter.style.color = 'green'
+    }
+}
